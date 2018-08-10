27aab15a52703469b9fe3f59a8ebbeec3df85a7f
Ben <zyhfish@163.com>
2018-08-10 11:21:57 +0800

    add license header.

--- SQLAzureMigration/SQLAzureMWUtils/ServerConnect.cs
+++ SQLAzureMigration/SQLAzureMWUtils/ServerConnect.cs
@@ -1,23 +1,23 @@
-﻿/*============================================================================
-  File:    ServerConnect.cs 
-
-  Summary: Implements a sample SMO server connection utility in C#.
-
-  Date:    June 06, 2005
-------------------------------------------------------------------------------
-  This file is part of the Microsoft SQL Server Code Samples.
-
-  Copyright (C) Microsoft Corporation.  All rights reserved.
-
-  This source code is intended only as a supplement to Microsoft
-  Development Tools and/or on-line documentation.  See these other
-  materials for detailed information regarding Microsoft code samples.
-
-  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
-  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-  PARTICULAR PURPOSE.
-============================================================================*/
+﻿#region Copyright
+// 
+// DotNetNuke® - http://www.dotnetnuke.com
+// Copyright (c) 2002-2018
+// by DotNetNuke Corporation
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+#endregion
 
 #region Using directives
 
